e20ac1b16a1b4870727afc4bbbf314e1b2673dfb
Jonathan Merritt <j.s.merritt@gmail.com>
2023-12-28 14:45:42 +1100

    Move checkerboard config into type

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,29 @@ module Main where
 import Data.Word (Word16, Word8)
 import Squizz.PPM (writeRasterPPM)
 import Squizz.Rasterize (rasterizeSimple)
-import Squizz.TestImages (checkerBoard)
+import Squizz.TestImages
+  ( CheckerBoardConfig
+      ( CheckerBoardConfig,
+        color1,
+        color2,
+        rectHeight,
+        rectWidth
+      ),
+    checkerBoard,
+  )
 import Squizz.Types (Point2D, RGB (RGB))
 
 main :: IO ()
 main =
-  let cb :: Point2D Float -> RGB Word8
-      cb = checkerBoard (RGB 255 255 255) (RGB 0 0 0) 16 16
+  let cfg :: CheckerBoardConfig Float (RGB Word8)
+      cfg =
+        CheckerBoardConfig
+          { color1 = RGB 255 255 255,
+            color2 = RGB 0 0 0,
+            rectWidth = 16,
+            rectHeight = 16
+          }
 
-      cb' :: Point2D Word16 -> RGB Word8
-      cb' = cb . fmap fromIntegral
-   in writeRasterPPM "checkerboard.ppm" $ rasterizeSimple 128 64 cb'
+      cb :: Point2D Word16 -> RGB Word8
+      cb = checkerBoard cfg . fmap fromIntegral
+   in writeRasterPPM "checkerboard.ppm" $ rasterizeSimple 128 64 cb
